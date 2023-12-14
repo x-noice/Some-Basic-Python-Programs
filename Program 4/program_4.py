05a33459beab57a38b4456d00e0b250d5165c7c5
x-noice <150224928+x-noice@users.noreply.github.com>
2023-12-14 13:55:54 +0530

    added correct programs

--- Program 4/program_4.py
+++ Program 4/program_4.py
@@ -1,5 +1,23 @@
-original_cost = eval(input('Enter the original cost of the goods:\n₹'))
-gst_rate = eval(input('Enter the gst rate %: \n'))
-gst_amount = (original_cost * gst_rate)/100
-total_amt = original_cost + gst_amount
-print('Total amount is ₹', total_amt, sep = '')
+num = int(input('Enter a 1 digit number: '))
+if(num == 0):
+    print('Zero')
+elif(num == 1):
+    print('One')
+elif(num == 2):
+    print('Two')
+elif(num == 3):
+    print('Three')
+elif(num == 4):
+    print('Four')
+elif(num == 5):
+    print('Five')
+elif(num == 6):
+    print('Six')
+elif(num == 7):
+    print('Seven')
+elif(num == 8):
+    print('Eight')
+elif(num == 9):
+    print('Nine')
+else:
+    print('Please enter a 1 digit integer.')
--- Program 5/program_5.py
+++ Program 5/program_5.py
@@ -1,24 +1,21 @@
-#WAP input digit print the number in words.
-num = int(input('Enter a 1 digit number:\n'))
-if(num == 0):
-    print('Zero')
-elif(num == 1):
-    print('One')
-elif(num == 2):
-    print('Two')
-elif(num == 3):
-    print('Three')
-elif(num == 4):
-    print('Four')
-elif(num == 5):
-    print('Five')
-elif(num == 6):
-    print('Six')
-elif(num == 7):
-    print('Seven')
-elif(num == 8):
-    print('Eight')
-elif(num == 9):
-    print('Nine')
+marks1 = int(input("Enter the marks of 1 subject: "))
+marks2 = int(input("Enter the marks of 2 subject: "))
+marks3 = int(input("Enter the marks of 3 subject: "))
+print("Average marks:", round((marks1+marks2+marks3)/3, 2))
+percentage = (marks1+marks2+marks3)/300*100
+round_percentage = round(percentage)
+print("Percentage: ", round_percentage,'%',sep='')
+if round_percentage >= 80:
+    print("Grade A", "level 4, above agency-normalized standards")
+elif round_percentage >= 70 and round_percentage <= 79:
+    print("Grade B", "level 3, at agency-normalized standards")
+elif round_percentage >= 60 and round_percentage <= 69:
+    print("Grade C", "level 2, below,but approaching agency-normalized standards")
+elif round_percentage >= 50 and round_percentage <= 59:
+    print("Grade D", "level 1, well below agency-normalized standards")
+elif round_percentage >= 40 and round_percentage <= 49:
+    print("Grade E", "level 1-, too below agency-normalized standards")
+elif round_percentage <= 39:
+    print("Grade R", "Remedial standards")
 else:
-    print('Please enter a 1 digit integer')
+    print('An error occurred.')
